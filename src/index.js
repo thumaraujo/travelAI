40f1c334e7d39b21a7ddd3c5720629191742d214
thumaraujo <165036906+thumaraujo@users.noreply.github.com>
2024-06-15 20:16:51 +0100

    created HTML, CSS and JS

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function displayTravel(response) {
+  new Typewriter("#travel", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generateTravel(event) {
+  event.preventDefault();
+
+  let promptInput = document.querySelector("#user-prompt");
+  let apiKey = "406tac3d1af0db7dfbb34a6c42bb8ofe";
+  let context =
+    "You're a travel specialist, you know details about places all over the world and the best things to do in each one. Write using HTML and <br />.";
+  let prompt = `Write an answer about things to do, focusing on ${promptInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let travelElement = document.querySelector("#travel");
+  travelElement.classList.remove("hidden");
+  travelElement.innerHTML = `<div class="creating"> ✍️ Researching about ${promptInput.value}</div>`;
+  axios.get(apiURL).then(displayTravel);
+}
+
+let travelFormElement = document.querySelector("#travel-form");
+travelFormElement.addEventListener("submit", generateTravel);
